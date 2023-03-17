7e5f216de5db4138a839db471b809f0774763f27
esgadom <estgadom@gmail.com>
2023-03-17 02:58:55 +0100

    Plantilla consultas 4 sql

---
+++ sql/leccion-12-consultas-multiples-tablas-4.sql
@@ -0,0 +1,28 @@
+/* Pair programming Consultas en múltiples tablas 4 */
+-- ------------------------------------------------------------------------------------------------------------------------------------
+
+/*Es el turno de las subqueries. En este ejercicios os planteamos una serie de queries que nos permitirán conocer información de la base de datos, 
+que tendréis que solucionar usando subqueries.*/
+
+/*1. Extraed información de los productos "Beverages"
+En este caso nuestro jefe nos pide que le devolvamos toda la información necesaria para identificar un tipo de producto. 
+En concreto, tienen especial interés por los productos con categoría "Beverages". Devuelve el ID del producto, el nombre del producto y su ID de categoría.
+La query debería resultar en una tabla como esta:*/
+
+/*2. Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país
+Suponemos que si se trata de ofrecer un mejor tiempo de entrega a los clientes, entonces podría dirigirse a estos países para buscar proveedores adicionales.
+Los resultados de esta query son:*/
+
+/*3. Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
+Extraed el OrderId y el nombre del cliente que pidieron más de 20 artículos del producto "Grandma's Boysenberry Spread" (ProductID 6) en un solo pedido.
+Resultado de nuestra query deberíamos tener una tabla como esta:*/
+
+/*4. Extraed los 10 productos más caros
+Nos siguen pidiendo más queries correlacionadas. En este caso queremos saber cuáles son los 10 productos más caros.
+Los resultados esperados de esta query son:*/
+
+/*5. BONUS:
+Qué producto es más popular
+Extraed cuál es el producto que más ha sido comprado y la cantidad que se compró.
+El resultado de esta query es:*/
+
