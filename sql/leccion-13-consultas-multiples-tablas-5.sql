5e3b08dc88d2a304a71a95100852a3ea99ec8b6d
MaitaneP <portillamaitane@gmail.com>
2023-03-17 08:35:56 +0100

    plantilla 13 sql y 14 python. Bonus leccion11 python

---
+++ sql/leccion-13-consultas-multiples-tablas-5.sql
@@ -0,0 +1,44 @@
+/* Pair programming Consultas en múltiples tablas 5 */
+-- ------------------------------------------------------------------------------------------------------------------------------------
+
+/*En esta lección de pair programming vamos a continuar trabajando sobre la base de datos Northwind. En este caso trabajaremos 
+con queries correlacionadas y con estamenteos como el like y not like. Para solucionar algunos de los ejercicios tendremos que 
+aplicar conocimientos apredidos previamente como los join.
+Es el momento de poner práctica los conceptos aprendidos en la lección de queries múltiples V.*/
+
+
+
+/*1. Extraed los pedidos con el máximo "order_date" para cada empleado.
+Nuestro jefe quiere saber la fecha de los pedidos más recientes que ha gestionado cada empleado. Para eso nos pide que lo hagamos 
+con una query correlacionada.*/
+
+
+
+/*2. Extraed el precio unitario máximo (unit_price) de cada producto vendido.
+Supongamos que ahora nuestro jefe quiere un informe de los productos vendidos y su precio unitario. De nuevo lo tendréis que hacer 
+con queries correlacionadas.*/
+
+
+
+/*3. Ciudades que empiezan con "A" o "B".
+Por un extraño motivo, nuestro jefe quiere que le devolvamos una tabla con aquelas compañias que están afincadas en ciudades que 
+empiezan por "A" o "B". Necesita que le devolvamos la ciudad, el nombre de la compañia y el nombre de contacto.*/
+
+
+
+/*4. Número de pedidos que han hecho en las ciudades que empiezan con L.
+En este caso, nuestro objetivo es devolver los mismos campos que en la query anterior el número de total de pedidos que han hecho 
+todas las ciudades que empiezan por "L".*/
+
+
+
+/*5. Todos los clientes cuyo "contact_title" no incluya "Sales".
+Nuestro objetivo es extraer los clientes que no tienen el contacto "Sales" en su "contact_title". Extraer el nombre de contacto, 
+su posisión (contact_title) y el nombre de la compañia.*/
+
+
+
+/*6. Todos los clientes que no tengan una "A" en segunda posición en su nombre.
+Devolved unicamente el nombre de contacto.*/
+
+
